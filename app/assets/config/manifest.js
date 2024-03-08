b1ace8a3eeca031518a34549b018c3cd84800d4a
Daniel Pierce <dlpierce@indiana.edu>
2024-03-08 16:26:11 -0500

    Generated Hyrax 5.0.0 app

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+
+//= link openseadragon-assets
+
+//= link blacklight_gallery/manifest.js
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,15 @@
+//= require jquery3
+//= require jquery.dataTables
+//= require dataTables.bootstrap4
+//= require rails-ujs
+//= require turbolinks
+//
+// Required by Blacklight
+//= require popper
+// Twitter Typeahead for autocomplete
+//= require twitter/typeahead
+//= require bootstrap
+//= require blacklight/blacklight
+//= require blacklight_gallery
+//= require hyrax
+
