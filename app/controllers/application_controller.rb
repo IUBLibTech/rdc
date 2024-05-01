f5008f83d8865ea381e7f14dc7e577e973f8dfd4
Daniel Pierce <dlpierce@indiana.edu>
2024-05-01 15:21:09 -0400

    Revert "Add fix for rails engine route helpers with relative_url_root"
    This reverts commit 2c0b983881b718a37bd2e59eb3f3a9f445ad7677.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,5 @@ class ApplicationController < ActionController::Base
   include Hyrax::ThemedLayoutController
   with_themed_layout '1_column'
 
-  # Fixes engine route helpers (e.g. hyrax.about_path) ignoring relative_url_root path prefix
-  def url_options
-    options = super
-    script_name = [Rails.application.config.relative_url_root, options[:script_name]].join('')
-    options.merge(script_name: script_name)
-  end
+
 end
