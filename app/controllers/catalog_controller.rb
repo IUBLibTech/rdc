2128303b363997950ef578c6bc23df7f28ac52bb
Adam Ploshay <aploshay@iu.edu>
2024-12-11 18:34:05 -0500

    [IULRDC-28] in catalog index, render campus display values instead of codes

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -85,7 +85,7 @@ class CatalogController < ApplicationController
     config.add_index_field "title_tesim", label: "Title", itemprop: 'name', if: false
     config.add_index_field "abstract_tesim", label: "Summary",  itemprop: 'summary', helper_method: :render_markdown
     config.add_index_field "domain_subject_tesim", label: 'Domain Subject', itemprop: 'keywords', link_to_facet: "domain_subject_sim"
-    config.add_index_field "campus_tesim", label: 'Campus', itemprop: 'campus', link_to_facet: "campus_sim"
+    config.add_index_field "campus_tesim", label: 'Campus', itemprop: 'campus', link_to_facet: "campus_sim", helper_method: :campus_helper
     config.add_index_field "holding_location_tesim", label: 'Hosting Unit', link_to_facet: "holding_location_sim"
     config.add_index_field "rights_statement_tesim", label: 'Access Restrictions', link_to_facet: 'rights_statement_sim', helper_method: :rights_statement_links
     # config.add_index_field "subject_tesim", itemprop: 'about', link_to_facet: "subject_sim"
