90a9f2a116b4249bf7b916171791b406066f00f6
Adam Ploshay <aploshay@iu.edu>
2026-08-20 11:20:14 -0400

    [IULRDC-244] limit catalog results per page as per essi#754

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -5,6 +5,7 @@ class CatalogController < ApplicationController
 
   # This filter applies the hydra access controls
   before_action :enforce_show_permissions, only: :show
+  before_action :limit_per_page, only: :index
 
   def self.uploaded_field
     "system_create_dtsi"
@@ -40,7 +41,7 @@ class CatalogController < ApplicationController
 
     config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_results_collection_tool(:sort_widget)
-    config.add_results_collection_tool(:per_page_widget)
+    config.add_results_collection_tool(:per_page_widget, if: :current_user)
     config.add_results_collection_tool(:view_type_group)
     config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
@@ -333,5 +334,10 @@ class CatalogController < ApplicationController
   # disable querying, storing blacklight Search records which are never used
   def find_search_session
     return nil
+  end
+
+  private
+  def limit_per_page
+    blacklight_config.max_per_page = 10 unless current_user.present?
   end
 end
